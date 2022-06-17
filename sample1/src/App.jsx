fccef160a3b2eccbacb8f18fa432d3e82ae2a71c
Shanu Jha <shanu.j@foxberry.in>
2022-06-17 19:49:44 +0000

    added basic routing

--- sample1/src/App.jsx
+++ sample1/src/App.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+const Navbar = <nav style={{
+  height: '50px',
+  background: '#f3f3f3',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'flex-end',
+  alignItems: 'center'
+}}>
+  <Link style={{ paddingRight: 10 }} to="/">Home</Link>
+  <Link style={{ paddingRight: 10 }} to="/about">About</Link>
 
+</nav>;
 const App = () => {
-  return <div>Hello React App!</div>
+  return <BrowserRouter>
+    {Navbar}
+    <Routes>
+      <Route path="/about" element={<About/>}></Route>
+      <Route path="/" element={<Home/>}></Route>
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App;
+
+const Home = () => {
+  return <div>Home</div>
+}
+
+const About = () => {
+  return <div>About</div>
+}
